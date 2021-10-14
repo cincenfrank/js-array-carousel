4ef2eb57df499e6f28e7a9d33177d40148e1659c
cincenfrank <fpetrangeli1@gmail.com>
2021-10-14 17:44:37 +0200

    added validation data condition

--- js/script.js
+++ js/script.js
@@ -25,105 +25,110 @@ const buttonDown = document.querySelector(".carousel-controller.down-arrow");
 
 let activeIndex = 0;
 
-// LOADING LOOP
-for (let i = 0; i < items.length; i++) {
-  const singleImage = items[i];
-  const singleTitle = title[i];
-  const singleText = text[i];
+// page validation logic
+if (items.length === text.length && items.length === title.length) {
+  // LOADING LOOP
+  for (let i = 0; i < items.length; i++) {
+    const singleImage = items[i];
+    const singleTitle = title[i];
+    const singleText = text[i];
 
-  //   MAIN IMAGE LOGIC
-  const mainImage = document.createElement("div");
-  mainImage.classList.add("main-image");
-  if (i === activeIndex) {
-    mainImage.classList.add("active");
+    //   MAIN IMAGE LOGIC
+    const mainImage = document.createElement("div");
+    mainImage.classList.add("main-image");
+    if (i === activeIndex) {
+      mainImage.classList.add("active");
+    }
+
+    const imgElement = document.createElement("img");
+    imgElement.src = singleImage;
+    imgElement.alt = `${singleTitle} image`;
+
+    const descriptionBlock = document.createElement("div");
+    descriptionBlock.classList.add("description");
+
+    const descriptionTitle = document.createElement("h2");
+    descriptionTitle.classList.add("px-3", "my-0", "fs-4");
+    descriptionTitle.textContent = singleTitle;
+
+    const descriptionText = document.createElement("p");
+    descriptionText.classList.add("px-3", "mb-4", "fs-5");
+    descriptionText.textContent = singleText;
+
+    descriptionBlock.appendChild(descriptionTitle);
+    descriptionBlock.appendChild(descriptionText);
+
+    mainImage.appendChild(imgElement);
+    mainImage.appendChild(descriptionBlock);
+
+    stackedMainContainer.appendChild(mainImage);
+
+    //   THUMBS COLUMN LOGIC
+    const thumbBlock = document.createElement("div");
+    thumbBlock.classList.add("ratio", "ratio-21x9", "thumb-block");
+    if (i === activeIndex) {
+      thumbBlock.classList.add("active");
+    }
+    thumbBlock.setAttribute("index", i.toString());
+    const thumbImg = document.createElement("img");
+    thumbImg.classList.add("w-100", "thumb");
+    thumbImg.src = singleImage;
+    thumbImg.alt = `${singleTitle} thumb`;
+
+    const thumbBorder = document.createElement("div");
+    thumbBorder.classList.add("thumb-border");
+    const thumbOverlay = document.createElement("div");
+    thumbOverlay.classList.add("thumb-overlay");
+
+    thumbBlock.appendChild(thumbImg);
+    thumbBlock.appendChild(thumbBorder);
+    thumbBlock.appendChild(thumbOverlay);
+
+    thumbColumn.appendChild(thumbBlock);
   }
 
-  const imgElement = document.createElement("img");
-  imgElement.src = singleImage;
-  imgElement.alt = `${singleTitle} image`;
-
-  const descriptionBlock = document.createElement("div");
-  descriptionBlock.classList.add("description");
-
-  const descriptionTitle = document.createElement("h2");
-  descriptionTitle.classList.add("px-3", "my-0", "fs-4");
-  descriptionTitle.textContent = singleTitle;
-
-  const descriptionText = document.createElement("p");
-  descriptionText.classList.add("px-3", "mb-4", "fs-5");
-  descriptionText.textContent = singleText;
-
-  descriptionBlock.appendChild(descriptionTitle);
-  descriptionBlock.appendChild(descriptionText);
-
-  mainImage.appendChild(imgElement);
-  mainImage.appendChild(descriptionBlock);
-
-  stackedMainContainer.appendChild(mainImage);
-
-  //   THUMBS COLUMN LOGIC
-  const thumbBlock = document.createElement("div");
-  thumbBlock.classList.add("ratio", "ratio-21x9", "thumb-block");
-  if (i === activeIndex) {
-    thumbBlock.classList.add("active");
-  }
-  thumbBlock.setAttribute("index", i.toString());
-  const thumbImg = document.createElement("img");
-  thumbImg.classList.add("w-100", "thumb");
-  thumbImg.src = singleImage;
-  thumbImg.alt = `${singleTitle} thumb`;
-
-  const thumbBorder = document.createElement("div");
-  thumbBorder.classList.add("thumb-border");
-  const thumbOverlay = document.createElement("div");
-  thumbOverlay.classList.add("thumb-overlay");
-
-  thumbBlock.appendChild(thumbImg);
-  thumbBlock.appendChild(thumbBorder);
-  thumbBlock.appendChild(thumbOverlay);
-
-  thumbColumn.appendChild(thumbBlock);
+  buttonUp.addEventListener("click", function () {
+    if (activeIndex === 0) {
+      activeIndex = items.length - 1;
+    } else {
+      activeIndex--;
+    }
+    //   MAIN IMAGE LOGIC
+    const previousMainImage = document.querySelector(".main-image.active");
+    previousMainImage.classList.remove("active");
+    const newMainImage = document.querySelector(
+      `.main-image:nth-child(${activeIndex + 1})`
+    );
+    newMainImage.classList.add("active");
+    // THUMB LOGIC
+    const previousThumbBlock = document.querySelector(".thumb-block.active");
+    previousThumbBlock.classList.remove("active");
+    const newThumbBlock = document.querySelector(
+      `.thumb-block[index="${activeIndex}"]`
+    );
+    newThumbBlock.classList.add("active");
+  });
+  buttonDown.addEventListener("click", function () {
+    if (activeIndex === items.length - 1) {
+      activeIndex = 0;
+    } else {
+      activeIndex++;
+    }
+    //   MAIN IMAGE LOGIC
+    const previousMainImage = document.querySelector(".main-image.active");
+    previousMainImage.classList.remove("active");
+    const newMainImage = document.querySelector(
+      `.main-image:nth-child(${activeIndex + 1})`
+    );
+    newMainImage.classList.add("active");
+    // THUMB LOGIC
+    const previousThumbBlock = document.querySelector(".thumb-block.active");
+    previousThumbBlock.classList.remove("active");
+    const newThumbBlock = document.querySelector(
+      `.thumb-block[index="${activeIndex}"]`
+    );
+    newThumbBlock.classList.add("active");
+  });
+} else {
+  alert("input lists have different sizes. Impossible to continue");
 }
-
-buttonUp.addEventListener("click", function () {
-  if (activeIndex === 0) {
-    activeIndex = items.length - 1;
-  } else {
-    activeIndex--;
-  }
-  //   MAIN IMAGE LOGIC
-  const previousMainImage = document.querySelector(".main-image.active");
-  previousMainImage.classList.remove("active");
-  const newMainImage = document.querySelector(
-    `.main-image:nth-child(${activeIndex + 1})`
-  );
-  newMainImage.classList.add("active");
-  // THUMB LOGIC
-  const previousThumbBlock = document.querySelector(".thumb-block.active");
-  previousThumbBlock.classList.remove("active");
-  const newThumbBlock = document.querySelector(
-    `.thumb-block[index="${activeIndex}"]`
-  );
-  newThumbBlock.classList.add("active");
-});
-buttonDown.addEventListener("click", function () {
-  if (activeIndex === items.length - 1) {
-    activeIndex = 0;
-  } else {
-    activeIndex++;
-  }
-  //   MAIN IMAGE LOGIC
-  const previousMainImage = document.querySelector(".main-image.active");
-  previousMainImage.classList.remove("active");
-  const newMainImage = document.querySelector(
-    `.main-image:nth-child(${activeIndex + 1})`
-  );
-  newMainImage.classList.add("active");
-  // THUMB LOGIC
-  const previousThumbBlock = document.querySelector(".thumb-block.active");
-  previousThumbBlock.classList.remove("active");
-  const newThumbBlock = document.querySelector(
-    `.thumb-block[index="${activeIndex}"]`
-  );
-  newThumbBlock.classList.add("active");
-});
